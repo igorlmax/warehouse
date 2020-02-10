21223ab9600a69525950ecc32e89449b3c488c2f
Igor <igorlmax@yahoo.com>
2020-02-10 14:22:05 +0100

    *define LogItem.js *based on log.attention change the color *style and display props on LogItem.js

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 const SearchBar = () => {
   return (
-    <nav style={{marginBottom: '30px'}} className='blue'>
+    <nav style={{marginBottom: '30px'}} className='accent'>
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import LogItem from "./LogItem";
 
 const Logs = () => {
-
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -11,7 +11,7 @@ const Logs = () => {
 
   const getLogs = async () => {
     setLoading(true);
-    const res = await fetch('/logs');
+    const res = await fetch("/logs");
     const data = await res.json();
 
     setLogs(data);
@@ -19,20 +19,22 @@ const Logs = () => {
   };
 
   if (loading) {
-    return <h4>Loading...</h4>
+    return <h4>Loading...</h4>;
   }
 
   return (
-  <div>
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
-      </li>
-      {!loading && logs.length === 0 ? (<p className="center">No logs to show...</p>) : (
-          logs.map(log => <li>{log.message}</li>)
-      )}
-    </ul>
-  </div>
+    <div className="container">
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4 className="center">System Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className="center">No logs to show...</p>
+        ) : (
+          logs.map(log => <LogItem log={log} key={log.id} />)
+        )}
+      </ul>
+    </div>
   );
 };
 
