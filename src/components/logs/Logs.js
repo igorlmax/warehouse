674c02acb4dc35ab94087c8703792494b45ea873
Igor <igorlmax@yahoo.com>
2020-02-11 13:27:21 +0100

    *connect Redux to Component *refactor Logs.js to fetch Data from Redux Store instead from the local State

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from "react";
 import LogItem from "./LogItem";
 import Preloader from "../layout/Prelaoder";
+import { connect } from "react-redux";
+import {getLogs} from "../../actions/logActions";
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
+// destructure logs and loading property from log store
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+
+  /* were defined for a local state*/
+  // const [logs, setLogs] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getLogs();
+    getLogs(); // getLogs comes from Redux Action, it should be also destructured into the func. argument
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch("/logs");
-    const data = await res.json();
+  /* was used for a local state*/
+  // const getLogs = async () => {
+  //   setLoading(true);
+  //   const res = await fetch("/logs");
+  //   const data = await res.json();
+  //
+  //   setLogs(data);
+  //   setLoading(false);
+  // };
 
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -39,4 +45,9 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+// define what we want to get from the state
+const mapStateToProps = state => ({
+  log: state.log // bring the whole state and destructure in the func. argument
+});
+
+export default connect(mapStateToProps, {getLogs})(Logs);
